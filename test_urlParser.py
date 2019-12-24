8bb6f81290eb817fe04644205913ed0da9d32f09
safak <safakciplak1990@gmail.com>
2019-12-24 22:05:23 +0300

    tdd url parser

---
+++ test_urlParser.py
@@ -0,0 +1,44 @@
+import unittest
+from urlParser import urlParser
+
+class basicProtocolParsing(unittest.TestCase):
+	def assertProtocol(self,url,expected_protocol):
+		purl = urlParser(url)
+		self.assertEqual(purl.protocol,expected_protocol)
+
+	def test_html_protocol(self):
+		self.assertProtocol('http://www.essex.ac.uk', 'http')
+
+	def test_ftp_protocol(self):
+		self.assertProtocol('ftp://ftp.essex.ac.uk', 'ftp')
+
+	def other_protocol(self):
+                self.assertProtocol('w61w://some.site.com', 'w61w')
+
+class basicSiteParsing(unittest.TestCase):
+	def assertSite(self,url,expected_site):
+		purl = urlParser(url)
+		self.assertEqual(purl.site,expected_site)
+
+	def test_site_without_path(self):
+		self.assertSite('http://www.essex.ac.uk', 'www.essex.ac.uk')
+	
+	def test_site_with_complex_path(self):
+                self.assertSite('http://www.essex.ac.uk/csee/home.html', 'www.essex.ac.uk')
+	
+class basicPathParsing(unittest.TestCase):
+	def assertPath(self,url,expected_path):
+                purl = urlParser(url)
+                self.assertEqual(purl.path,expected_path)
+
+	def test_empty_path(self):
+                self.assertPath('http://www.essex.ac.uk', '')
+
+	def test_empty_path_with_a_slash(self):
+		self.assertPath('http://www.essex.ac.uk/', '')
+	
+	def test_empty_path_with_one_word(self):
+		self.assertPath('http://www.essex.ac.uk/homework', 'homework')
+
+	def test_empty_path_wtih_sub_folders(self):
+		self.assertPath('http://www.essex.com/homework/joy.txt', 'homework/joy.txt')
---
+++ urlParser.py
@@ -0,0 +1,6 @@
+class urlParser:
+	def __init__(self, url):
+		self.protocol, site_and_path = url.split('://')
+		site_and_path_list = site_and_path.split('/', 1)
+		self.site = site_and_path_list[0]
+		self.path = '/'.join(site_and_path_list[1:])
